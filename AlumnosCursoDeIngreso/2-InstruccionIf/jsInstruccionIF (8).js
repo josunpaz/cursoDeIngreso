3f4c824778079dcb20e8e8abadf7e7ef0d26ff64
josunpaz <josueds@hotmail.com.ar>
2016-07-06 22:11:46 -0300

    edicion 6

--- AlumnosCursoDeIngreso/2-InstruccionIf/jsInstruccionIF (8).js
+++ AlumnosCursoDeIngreso/2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,14 @@
 function Mostrar()
 {
 //tomo la edad  
+var estadoCivil=document.getElementById('estadoCivil').value;
+//alert(estadoCivil);
 
 	
+if (estadoCivil=="Casado"&& edad>18)
+{
 
+
+}
 
 }//FIN DE LA FUNCIÓN
